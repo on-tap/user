05ffbe8a39ebb9a51950b9a31f039c0e3d08e4e4
phuoc1707 <tdphuoc77@gmail.com>
2021-10-30 21:25:44 +0700

    retry

--- src/main/java/com/userservice/restController/RestController.java
+++ src/main/java/com/userservice/restController/RestController.java
@@ -4,31 +4,29 @@ import com.userservice.entity.Order;
 import com.userservice.entity.User;
 import com.userservice.entity.VO;
 import com.userservice.reposiroty.Repository;
-import com.userservice.restTemplate.RestTemplateConfig;
+import com.userservice.service.service;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/user")
 public class RestController {
 
-    @Autowired
-    private Repository repository;
 
     @Autowired
-    private RestTemplate restTemplateConfig;
+private service service;
+
 
     @GetMapping("/{id}")
     public VO getById(@PathVariable Long id){
-        User user = repository.findById(id).get();
-
-        Order order=  restTemplateConfig.getForObject("/"+user.getOrderID(),Order.class);
-
-        return new VO(user,order);
+        return service.getById(id);
     }
+
+
 }
---
+++ src/main/java/com/userservice/service/service.java
@@ -0,0 +1,36 @@
+package com.userservice.service;
+
+import com.userservice.entity.Order;
+import com.userservice.entity.User;
+import com.userservice.entity.VO;
+import com.userservice.reposiroty.Repository;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class service {
+
+    @Autowired
+    private Repository repository;
+    @Autowired
+
+    private RestTemplate restTemplateConfig;
+
+    @Retry(name = "basic",fallbackMethod = "getUserByIDFallBack")
+    public VO getById( Long id){
+        User user = repository.findById(id).get();
+
+        Order order=  restTemplateConfig.getForObject("/"+user.getOrderID(),Order.class);
+
+        return new VO(user,order);
+    }
+    
+    public VO getUserByIDFallBack( Long id,RuntimeException e){
+        User user = repository.findById(id).get();
+        VO vo= new VO();
+        vo.setUser(user);
+        return vo;
+    }
+}
